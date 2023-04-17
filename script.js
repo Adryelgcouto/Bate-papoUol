70313e1844c76b143cb3df1f19a9a57b3a68900b
Adryelgcouto <adryelgcouto@gmail.com>
2023-04-17 14:33:21 -0300

    arrumei a opção de entrada

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 axios.defaults.headers.common['Authorization'] = 'vqAALG1liXR0iqBfIgoFaDyx'
-const pedirNome = prompt('Qual o seu nome')
 let mensagens = []
-const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
-promise.then(receberMensagensDoServidor)
-function receberMensagensDoServidor(resposta) {
-  mensagens = resposta.data
-  console.log(resposta)
-  renderizarMensagem()
+let nome = {
+  name: ''
 }
-function renderizarMensagem() {
-  const mensagem = document.querySelector('ul')
-  for (let i = 0; i < mensagens.length; i++) {
-    const informacoesMensagens = mensagens[i]
-    mensagem.innerHTML += ` 
+
+//nome do Usuario
+function pedirNome() {
+  const nomeDoUsuario = prompt('Qual o seu nome')
+  const enviarDoUsuario = (nome.name = nomeDoUsuario)
+  console.log(nome)
+  const requisicao = axios
+    .post('https://mock-api.driven.com.br/api/vm/uol/participants', nome)
+    .then(response => {
+      setInterval(MensagemServidor, 3000)
+    })
+  requisicao.catch(UsuarioErro)
+  MensagemServidor()
+}
+pedirNome()
+function MensagemServidor() {
+  axios
+    .get('https://mock-api.driven.com.br/api/vm/uol/messages')
+    .then(response => {
+      response.data.forEach(({ from, to, text, type, time }) => {
+        const mensagem = document.querySelector('ul')
+        mensagem.innerHTML += ` 
                       <li>
-                      <span class="clock">(09:22:28)</span> <strong>${informacoesMensagens.from}</strong> para <strong>todos:</strong>${informacoesMensagens.text}
+                      <span class="clock">${time}</span> <strong>${from}</strong> para <strong>${to}</strong>${text}
                       </li>`
-  }
+      })
+    })
 }
+function UsuarioErro(erro) {
+  console.log('Deu erro')
+}
+
 function enviarMensagem() {
   const input = document.querySelector('input')
   const mensagemNova = {
-    from: pedirNome,
+    from: '',
     to: 'todos',
     text: input.value,
     type: 'message'
